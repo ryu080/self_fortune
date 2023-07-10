700f09164c42a6def60d6e4f084802e047d8d916
ryu080 <130375359+ryu080@users.noreply.github.com>
2023-07-10 12:50:03 +0900

    'Google-Mobile-Ads-SDK'を再度インストール
    cocoapodsアップデート後何度もエラーが発生したため

--- Pods/Target Support Files/Pods-self_fortune/Pods-self_fortune-acknowledgements.markdown
+++ Pods/Target Support Files/Pods-self_fortune/Pods-self_fortune-acknowledgements.markdown
@@ -3,7 +3,7 @@ This application makes use of the following third party libraries:
 
 ## Google-Mobile-Ads-SDK
 
-Copyright 2022 Google
+Copyright 2023 Google
 
 ## GoogleAppMeasurement
 
--- self_fortune/AppDelegate.swift
+++ self_fortune/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         // 2秒の待機時間を設定
         Thread.sleep(forTimeInterval: 1.0)
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
 
